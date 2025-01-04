3586f3c78f77aa1d1727e32d1e9e3397b6bd66d9
Ayush Sharma <asayush35@gmail.com>
2025-01-04 23:11:44 +0530

    lombok annotations not working properly so doing it manually

--- src/main/java/com/personal/expensetracker/expensetracker/Expense.java
+++ src/main/java/com/personal/expensetracker/expensetracker/Expense.java
@@ -10,9 +10,41 @@ import java.time.LocalDate;
 @Entity
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
+//@NoArgsConstructor
 @Table(name = "service_expense")
 public class Expense {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
@@ -27,5 +59,8 @@ public class Expense {
         this.description = description;
         this.amount = amount;
         this.date = date;
+    }
+
+    public Expense(){
     }
 }
