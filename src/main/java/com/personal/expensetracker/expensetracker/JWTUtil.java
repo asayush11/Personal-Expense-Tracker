1df6ac435c3179910db74b8079865cc1a9923523
Ayush Sharma <asayush35@gmail.com>
2025-01-08 22:38:08 +0530

    Making JWT key independent of application startup

--- src/main/java/com/personal/expensetracker/expensetracker/JWTUtil.java
+++ src/main/java/com/personal/expensetracker/expensetracker/JWTUtil.java
@@ -3,15 +3,26 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JWTUtil {
 
-    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    @Value("${jwt.secret}")
+    private String secretKey;
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
 
     public String generateToken(String email) {
         var expirationTime = 3600000;
